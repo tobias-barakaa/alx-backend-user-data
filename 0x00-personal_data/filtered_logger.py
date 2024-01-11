8b8cda2c3be650f282c29bcd2007129003a64215
tobias-barakaa <ngairato@gmail.com>
2024-01-11 11:04:52 -0800

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,8 @@ def filter_datum(fields, redaction, message, separator):
     """Obfuscates specified fields in a
     log message using a regex substitution.
     """
-    pattern = r"{}=(.*?)(?={})".format("|".join(fields), separator)
-    return re.sub(pattern, lambda m: m.group(1).replace(m.group(2), redaction), message)
-
+    return re.sub(
+        rf'(?<=\b{"|".join(fields)}=)[^{separator}]+',
+        redaction,
+        message
+    )
