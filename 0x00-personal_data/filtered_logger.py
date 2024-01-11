2a586fef86f94a27e543bfe500c0fca556932d72
tobias-barakaa <ngairato@gmail.com>
2024-01-11 11:36:17 -0800

    filtered_logger_py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields:List[str], redaction:str,
-                 message:str, separator:str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
         function: filter_datum
     """
