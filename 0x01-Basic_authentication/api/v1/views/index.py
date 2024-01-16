0a2bfb71cf99df342c4240430098a4f04399325d
tobias-barakaa <ngairato@gmail.com>
2024-01-16 12:48:35 -0800

    index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+def unauthorized() -> None:
     """ GET /api/v1/unauthorized
     Return:
       - abort with 401 status
