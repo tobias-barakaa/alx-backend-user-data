152bb052254ada5eb6891cb839ea8b3c1b1d253b
tobias-barakaa <ngairato@gmail.com>
2024-01-16 15:54:51 -0800

    forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -35,7 +35,7 @@ def unauthorized() -> None:
     abort(401)
 
 
-@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+@app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> str:
     """ GET /api/v1/forbidden
     Return:
