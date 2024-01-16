9148d0ce2723eee364b20b965e0019da3421f99a
tobias-barakaa <ngairato@gmail.com>
2024-01-16 13:34:55 -0800

    forgot a slash/

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> None:
     """ GET /api/v1/unauthorized
     Return:
