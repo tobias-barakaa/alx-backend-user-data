1e51e0c9fac2d3dfffd58d3ae2f9502f6c1ee774
tobias-barakaa <ngairato@gmail.com>
2024-01-16 16:34:32 -0800

    server was down

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,13 +8,10 @@ from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.basic_auth import BasicAuth
 auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
+if os.getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
-if auth_type == 'basic_auth':
-    auth = BasicAuth()
 
 
 app = Flask(__name__)
