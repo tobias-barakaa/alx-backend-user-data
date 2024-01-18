c535f86c5ee77431384f9aff284389d5f84534a5
Baraka Ngaira <ngairato@gmail.com>
2024-01-18 17:13:19 +0300

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+from models import db_session
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -54,22 +56,22 @@ class BasicAuth(Auth):
 
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
-        """ user_object_from_credentials
+        """Return the User instance based on email and password.
         """
-        if user_email is None or type(user_email) is not str:
+        if user_email is None or not isinstance(user_email, str):
             return None
-        if user_pwd is None or type(user_pwd) is not str:
+
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        from models import db_session
-        from models.user import User
-        users = db_session.query(User).filter_by(email=user_email).all()
-        if users is None or users == []:
+        users = User.search({"email": user_email})
+        if not users:
             return None
+        
         for user in users:
-            if not user.is_valid_password(user_pwd):
-                return None
-            else:
+            if user.is_valid_password(user_pwd):
                 return user
+            
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user
