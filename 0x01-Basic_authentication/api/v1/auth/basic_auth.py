c736dfb9475cdbf0f61be6fda4513340aed70afe
tobias-barakaa <ngairato@gmail.com>
2024-01-18 07:05:35 -0800

    truns out return None was inside the method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -66,7 +66,7 @@ class BasicAuth(Auth):
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
-            return None
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user
