ee884db8c4cde9f3d1739fd631603cd6a75982bf
Baraka Ngaira <ngairato@gmail.com>
2024-01-18 10:14:03 +0300

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -23,17 +23,15 @@ class BasicAuth(Auth):
 
     def decode_base64_authorization_header(self,
                                            base64_authorization_header: str
-                                           ) -> str:
-        """ decode_base64_authorization_header
-        """
-        if base64_authorization_header is None:
-            return None
-        if type(base64_authorization_header) is not str:
-            return None
-        try:
-            return base64_authorization_header.decode('utf-8')
-        except Exception:
-            return None
+                                           )-> str:
+    """ decode_base64_authorization_header """
+    if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
+        return None
+    try:
+        decoded_bytes = base64.b64decode(base64_authorization_header)
+        return decoded_bytes.decode('utf-8')
+    except Exception:
+        return None
 
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header: str
