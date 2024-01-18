1ab276e4979ecf4a86ede3276c96249ce2693db7
Baraka Ngaira <ngairato@gmail.com>
2024-01-18 22:09:13 +0300

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -68,11 +68,11 @@ class BasicAuth(Auth):
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Retrieve the User instance for a request."""
-        if request is None:
-            return None
+        """Retrieve User instance for a request."""
+        if request is None: return None
         auth_header = self.authorization_header(request)
         base64_auth_header = self.extract_base64_authorization_header(auth_header)
-        decoded_auth_header = self.decode_base64_authorization_header(base64_auth_header)
+        decoded_auth_header = self.decode_base64_authorization_header(
+            base64_auth_header)
         user_email, user_pwd = self.extract_user_credentials(decoded_auth_header)
         return self.user_object_from_credentials(user_email, user_pwd)
