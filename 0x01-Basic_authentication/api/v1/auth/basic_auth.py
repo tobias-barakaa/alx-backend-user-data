80ae32f6a6717e6cd9d7129c0cfb9cdc77dd467f
tobias-barakaa <ngairato@gmail.com>
2024-01-18 00:05:22 -0800

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -55,7 +55,8 @@ class BasicAuth(Auth):
     def user_object_from_credentials(self,
                                      user_email: str, user_pwd: str
                                      ) -> TypeVar('User'):
-    """ user_object_from_credentials """
+        """ user_object_from_credentials
+        """
     if user_email is None or not isinstance(user_email, str):
         return None
     if user_pwd is None or not isinstance(user_pwd, str):
