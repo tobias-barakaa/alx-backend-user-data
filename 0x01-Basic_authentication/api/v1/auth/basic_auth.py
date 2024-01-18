1310afecd6fc0a0e287ef593ccea8584a9148bef
tobias-barakaa <ngairato@gmail.com>
2024-01-18 00:10:31 -0800

    more errors

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,6 @@ from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 
-
 class BasicAuth(Auth):
     """ BasicAuth class
     """
@@ -38,6 +37,7 @@ class BasicAuth(Auth):
             return sample_string
         except Exception:
             return None
+        
 
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header: str
@@ -52,26 +52,24 @@ class BasicAuth(Auth):
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(':', 1))
 
-    def user_object_from_credentials(self,
-                                     user_email: str, user_pwd: str
-                                     ) -> TypeVar('User'):
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
         """ user_object_from_credentials
         """
-    if user_email is None or not isinstance(user_email, str):
-        return None
-    if user_pwd is None or not isinstance(user_pwd, str):
-        return None
-
-    users = db_session.query(User).filter_by(email=user_email).all()
-
-    if not users or len(users) == 0:
-        return None
-
-    for user in users:
-        if user.is_valid_password(user_pwd):
-            return user
-
-    return None
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        from models import db_session
+        from models.user import User
+        users = db_session.query(User).filter_by(email=user_email).all()
+        if users is None or users == []:
+            return None
+        for user in users:
+            if not user.is_valid_password(user_pwd):
+                return None
+            else:
+                return user
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user
@@ -83,4 +81,7 @@ class BasicAuth(Auth):
             auth_header)
         if base64_auth_header is None:
             return None
-        decoded_auth_header = self.decode_base64_author
+        decoded_auth_header = self.decode_base64_authorization_header(
+            base64_auth_header)
+        if decoded_auth_header is None:
+            return None
