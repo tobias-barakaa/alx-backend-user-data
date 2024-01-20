ec1c0b05cd5c317d082792ab8e99fb878396412c
tobias-barakaa <ngairato@gmail.com>
2024-01-20 12:35:39 -0800

    session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -30,3 +30,14 @@ def login() -> str:
     response = jsonify(users[0].to_json())
     response.set_cookie(os.getenv("SESSION_NAME"), session_id)
     return response
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ DELETE /api/v1/auth_session/logout
+    Return:
+      - empty JSON
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+    return jsonify({}), 200
