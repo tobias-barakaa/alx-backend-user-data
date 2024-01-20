e8e153be8d2640faa36d50c3743de59457b4b450
tobias-barakaa <ngairato@gmail.com>
2024-01-20 10:42:27 -0800

    I had  a continuation error

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,7 +58,7 @@ def before_request() -> None:
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None and \
-        auth.session_cookie(request) is None:
+            auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
