63e1693291be687b7f99cfaecd450676e7379685
tobias-barakaa <ngairato@gmail.com>
2024-01-24 07:33:36 -0800

    service_authentiation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,10 +33,10 @@ class Auth:
             raise ValueError(f'User {email} already exists')
 
     def valid_login(self, email: str, password: str) -> bool:
-        """Takes email and password required arguments and returns a boolean."""
+        """Takes email and password required arguments and returns a
+        boolean."""
         try:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
-
