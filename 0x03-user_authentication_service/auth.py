f3e98d68e7ce45a4b53004d93cfd585afdf3023d
tobias-barakaa <ngairato@gmail.com>
2024-01-24 07:46:13 -0800

    trying my best

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,12 +6,18 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
     """Takes in a password string arguments and returns bytes.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def _generate_uuid() -> str:
+    """Returns a string representation of a new UUID.
+    """
+    return str(uuid4())
 
 
 class Auth:
