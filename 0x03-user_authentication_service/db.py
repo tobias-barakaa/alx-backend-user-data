9b63f6ee95a729a97ac24f132d8206cc68105cd0
tobias-barakaa <ngairato@gmail.com>
2024-01-24 03:39:41 -0800

    pycodestyle

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,7 +46,7 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
-    
+
     def find_user_by(self, **kwargs) -> User:
         """Find a user in the database based on arbitrary keyword arguments.
 
