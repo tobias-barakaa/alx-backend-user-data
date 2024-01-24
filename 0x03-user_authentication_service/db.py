b2ae380e71d7668cdddfc014ae983f1a5bda81ee
Baraka Ngaira <ngairato@gmail.com>
2024-01-24 13:16:46 +0300

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,8 +4,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.session import Session
 from user import Base, User
 
 
