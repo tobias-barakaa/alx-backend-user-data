e1f4972dff1c9dae05a2813c5d1481294496dfc9
tobias-barakaa <ngairato@gmail.com>
2024-01-24 05:27:05 -0800

    Module

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,6 +17,23 @@ def welcome() -> str:
     """
     return jsonify({"message": "Bienvenue"}), 200
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """POST /users
+    JSON body:
+      - email
+      - password
+    Return:
+      - User object JSON represented
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
