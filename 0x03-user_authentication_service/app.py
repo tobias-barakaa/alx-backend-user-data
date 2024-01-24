8fd9ae9b200ed938f686caceb7fa9caa91308683
tobias-barakaa <ngairato@gmail.com>
2024-01-24 05:28:30 -0800

    did you like it

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,6 +17,7 @@ def welcome() -> str:
     """
     return jsonify({"message": "Bienvenue"}), 200
 
+
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
     """POST /users
