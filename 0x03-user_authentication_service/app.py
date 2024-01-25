2ca3319154f27a85a5ed7c38332ced4a68b14ad2
tobias-barakaa <ngairato@gmail.com>
2024-01-25 12:41:46 -0800

    profileg

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -81,5 +81,20 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """GET /profile
+    Return:
+      - User profile information JSON
+    """
+    session_id = request.cookies.get("session_id")
+    if session_id is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
