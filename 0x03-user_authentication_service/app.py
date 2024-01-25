3acd569fd6eaad45db013c9e786b718d4b3c8e64
tobias-barakaa <ngairato@gmail.com>
2024-01-25 01:29:09 -0800

    I hope this makes sene

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,7 +58,7 @@ def login() -> str:
     data = request.form
     email = data.get('email')
     password = data.get('password')
-    if valid_login(email, password):
+    if Auth.valid_login(email, password):
         session_id = AUTH.create_session(email)
         response = jsonify({"email": email, "message": "logged in"})
         response.set_cookie('session_id', session_id)
