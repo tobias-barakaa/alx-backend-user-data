37e850873b8bdacb7582b66d2a64a088bed3d346
tobias-barakaa <ngairato@gmail.com>
2024-01-25 06:01:24 -0800

    service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,17 +47,29 @@ def valid_login(email: str, password: str) -> bool:
 
 
 @app.route('/sessions', methods=['POST'])
-def login():
-    data = request.form
-    email = data.get('email')
-    password = data.get('password')
-    user = valid_login(email, password)
-    if user is None:
+def login() -> str:
+    """POST /sessions
+    JSON body:
+      - email
+      - password
+    Return:
+      - Session ID as JSON
+    """
+    try:
+        data = request.form
+        email = data.get('email')
+        password = data.get('password')
+
+        if valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie('session_id', session_id, httponly=True)
+            return response
+        else:
+            abort(401)
+
+    except Exception as e:
         abort(401)
-    session_id = AUTH.create_session(email)
-    response = jsonify({'email': user['email'], 'message': 'logged in'})
-    response.set_cookie('session_id', session_id)
-    return response
 
 
 if __name__ == "__main__":
