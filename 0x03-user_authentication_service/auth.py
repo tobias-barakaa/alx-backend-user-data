e36cdb11d1af29602a1302120979248fb6ce0cb0
tobias-barakaa <ngairato@gmail.com>
2024-01-25 06:37:24 -0800

    good thing about internet/Ai is that I don't have to say sorry everytime I repeat or ask the same question

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,7 +70,7 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
-    
+
     def destroy_session(self, user_id: int) -> None:
         """Takes a single user_id integer argument and returns None.
         """
