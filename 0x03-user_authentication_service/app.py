e21c56450eded88211a646bd31c54300feaa6e8a
tobias-barakaa <ngairato@gmail.com>
2024-01-25 00:13:53 -0800

    implementing login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,3 +34,22 @@ def users() -> str:
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """POST /sessions
+    JSON body:
+      - email
+      - password
+    Return:
+      - Session ID as JSON
+    """
+    email = request.form['email']
+    password = request.form['password']
+    if valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
