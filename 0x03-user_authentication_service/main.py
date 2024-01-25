861fc3e38b4bf64bc07e04f226285c9ba0c2651e
tobias-barakaa <ngairato@gmail.com>
2024-01-25 14:43:21 -0800

    main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -3,6 +3,7 @@
 """
 import requests
 
+
 def register_user(email: str, password: str) -> None:
     """Register user
     """
@@ -11,7 +12,8 @@ def register_user(email: str, password: str) -> None:
     response = requests.post(url + "/users", data=data)
     assert response.status_code == 200
     assert response.json() == {"email": email, "message": "user created"}
-    
+
+
 def log_in_wrong_password(email: str, password: str) -> None:
     """Log in with wrong password
     """
@@ -21,6 +23,7 @@ def log_in_wrong_password(email: str, password: str) -> None:
     assert response.status_code == 401
     assert response.json() == {"message": "wrong password"}
 
+
 def profile_unlogged() -> None:
     """Profile of unlogged user
     """
@@ -28,6 +31,7 @@ def profile_unlogged() -> None:
     response = requests.get(url + "/profile")
     assert response.status_code == 403
     assert response.json() == {"message": "Missing auth token"}
+
 
 def log_in(email: str, password: str) -> str:
     """Log in
@@ -41,23 +45,27 @@ def log_in(email: str, password: str) -> str:
     assert session_id is not None
     return session_id
 
+
 def profile_logged(session_id: str) -> None:
     """Profile of logged user
     """
     url = "http://your-web-server-base-url"
-    response = requests.get(url + "/profile", cookies={"session_id": session_id})
-    assert response.status_code == 200
-    assert response.json() == {"email": email}
+    data = requests.get(url + "/profile", cookies={"session_id": session_id})
+    assert data.status_code == 200
+    assert data.json() == {"email": email}
+
 
 def log_out(session_id: str) -> None:
     """Log out
     """
     url = "http://your-web-server-base-url"
-    response = requests.delete(url + "/sessions", cookies={"session_id": session_id})
-    assert response.status_code == 200
-    assert response.json() == {}
-    session_id = response.cookies.get("session_id")
+    dat = requests.delete(url + "/sessions",
+                          cookies={"session_id": session_id})
+    assert dat.status_code == 200
+    assert dat.json() == {}
+    session_id = dat.cookies.get("session_id")
     assert session_id is None
+
 
 def reset_password_token(email: str) -> str:
     """Reset password token
@@ -66,16 +74,19 @@ def reset_password_token(email: str) -> str:
     data = {"email": email}
     response = requests.post(url + "/reset_password", data=data)
     assert response.status_code == 200
-    assert response.json() == {"email": email, "message": "Reset password sent"}
+    assert response.json() == {"email": email,
+                               "message": "Reset password sent"}
     reset_token = response.json()["reset_token"]
     assert reset_token is not None
     return reset_token
+
 
 def update_password(email: str, reset_token: str, new_password: str) -> None:
     """Update password
     """
     url = "http://your-web-server-base-url"
-    data = {"email": email, "reset_token": reset_token, "new_password": new_password}
-    response = requests.put(url + "/reset_password", data=data)
-    assert response.status_code == 200
-    assert response.json() == {"email": email, "message": "Password updated"}
+    dat = {"email": email, "reset_token": reset_token,
+           "new_password": new_password}
+    data = requests.put(url + "/reset_password", dat=dat)
+    assert data.status_code == 200
+    assert data.json() == {"email": email, "message": "Password updated"}
