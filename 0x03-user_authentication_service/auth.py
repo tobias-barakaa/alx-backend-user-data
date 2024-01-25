cfa642027450ff9f7a3235665aca284bd7df53e4
tobias-barakaa <ngairato@gmail.com>
2024-01-25 14:00:04 -0800

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,3 +78,14 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             return None
+    
+    def get_reset_password_token(self, email: str) -> str:
+        """Takes an email string argument and returns a string.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
