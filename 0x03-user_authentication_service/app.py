465bbb81ac800352902aff885d7999f842d5c88f
tobias-barakaa <ngairato@gmail.com>
2024-01-25 12:24:04 -0800

    I cannot believe it

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,7 +46,7 @@ def valid_login(email: str, password: str) -> bool:
         return False
 
 
-@app.route('/sessions', methods=['POST'])
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """POST /sessions
     JSON body:
